789cb5b74dfb6f86a12ae6929f8baffa28bf581c
Documenter.jl <documenter@juliadocs.github.io>
2023-03-27 01:50:52 +0000

    build based on 4e0d2f5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OkTableTools","category":"page"},{"location":"#OkTableTools","page":"Home","title":"OkTableTools","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OkTableTools.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OkTableTools]","category":"page"}]
+}
